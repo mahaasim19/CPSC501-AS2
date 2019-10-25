303c85f4d0418849b857b460d71e5088e96f8d9b
mahaasim19 <47335314+mahaasim19@users.noreply.github.com>
2019-10-25 14:24:45 -0600

    Moved two methods inspectMethod and inspectConstructor from InpectorNew.java class

--- MethodInspector.java
+++ MethodInspector.java
@@ -7,57 +7,47 @@ import static java.lang.System.out;
 import static java.lang.System.err;
 
 
-public class MethodInspector extends FieldInspector{
+public class MethodInspector extends Inspector{
+
     Class clz;
-  	Constructor constructor;
-  	Object obj;
-  	Method meth;
+    public void inspectMethod(Object obj){
 
-    public MethodInspector(){
-
-    }
-
-
-    private void inspectMethod(Object obj){
-
-          System.out.println("Method:  ");
-          Method[] methods = clz.getDeclaredMethods();
-          for(Method m : methods ){
+        System.out.println("Method:  ");
+        Method[] methods = clz.getDeclaredMethods();
+        for(Method m : methods ){
             if(m.getDeclaringClass().equals(obj.getClass())){
-              // taken from TA example
-              System.out.println("   Modifier: "+ Modifier.toString(m.getModifiers())  + " \n   Return Type: "+ m.getReturnType() +   " \n   Name:"  + m.getName() );
-			        Parameter[] param = m.getParameters();
-			        out.println("Parameters: " );
+       // taken from TA example
+                out.println("   Modifier: "+ Modifier.toString(m.getModifiers())  + " \n   Return Type: "+ m.getReturnType() +   " \n   Name:"  + m.getName() );
+                Parameter[] param = m.getParameters();
+                System.out.println("Parameters: " );
 
-			        for(Parameter p:param){
-				          out.println("   Type: " + p.getType());
-				          out.println("   Name: " + p.getName());
-			        }
-			        System.out.println();
-		        }
+                for(Parameter p:param){
+                    out.println("   Type: " + p.getType());
+                    out.println("   Name: " + p.getName());
+                }
+            System.out.println();
+            }
 
             for (Class throwException : m.getExceptionTypes()) {
-						     System.out.println("Exception Throws: " + throwException.getName());
-					  }
+                System.out.println("Exception Throws: " + throwException.getName());
+            }
         }
-    }
-
-    private void inspectConstructor(Object obj){
-
-      System.out.println("Constructor:  ");
-      Constructor[] constructor = clz.getConstructors();
-      for(Constructor c : constructor){
-        out.println("   Modifier: "+ Modifier.toString(c.getModifiers()) + " \n   Name:"  + c.getName() );
-        Parameter[] param = c.getParameters();
-        out.println("Parameters: " );
-
-        for(Parameter p : param){
-            out.println("   Type: " + p.getType());
-            out.println("   Name: " + p.getName());
-        }
-        System.out.println();
       }
 
-    }
+      public void inspectConstructor(Object obj){
 
+          System.out.println("Constructor:  ");
+          Constructor[] constructor = clz.getConstructors();
+          for(Constructor c : constructor){
+              out.println("   Modifier: "+ Modifier.toString(c.getModifiers()) + " \n   Name:"  + c.getName() );
+              Parameter[] param = c.getParameters();
+              out.println("Parameters: " );
+
+              for(Parameter p : param){
+                  out.println("   Type: " + p.getType());
+                  out.println("   Name: " + p.getName());
+              }
+              System.out.println();
+            }
+        }
 }
