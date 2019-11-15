e3519b6b91e99187f048fa801e7172c8b280ac0c
mahaasim19 <47335314+mahaasim19@users.noreply.github.com>
2019-11-15 15:07:05 -0700

    add class createXMLFile for reading,writing, and printing

---
+++ createXMLFile.java
@@ -0,0 +1,36 @@
+
+import org.jdom.*;
+import java.io.*;
+import java.util.*;
+
+public class createXMLFile{
+
+
+      public static void writeXML(){
+
+      }
+
+      public static void readXML(){
+
+
+      }
+
+      public static void printXML(String fileName){
+
+        try{
+
+          SAXBuilder builder = new SAXBuilder();
+
+          Document document = builder.build(fileName);
+
+          Element element = document.getRootElement();
+        }
+        catch(Exception e){
+          e.printStackTrace();
+        }
+        catch(IOException ioe){
+          ioe.printStackTrace();
+        }
+
+      }
+}
