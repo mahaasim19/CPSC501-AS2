e27b45ea968be3efd990fc265a670bb3c613a626
mahaasim19 <47335314+mahaasim19@users.noreply.github.com>
2019-11-15 15:35:28 -0700

    added method called deserialize()

--- Deserialize.java
+++ Deserialize.java
@@ -1,6 +1,12 @@
-
+import java.util.*;
+import org.jdom.*;
+import java.io.*;
 
 public class Deserialize{
 
-
+      public static Object deserialize(Document document){
+        
+      }
+  
+  
 }
